7c47ee9ace6103afd96e60614db527e70d7dcc6b
Shivam Vashisth <120186345+ShivamVashisth28@users.noreply.github.com>
2025-11-12 00:22:33 +0530

    Update Pdf.jsx

--- frontend/src/pages/Pdf.jsx
+++ frontend/src/pages/Pdf.jsx
@@ -79,7 +79,7 @@ function Pdf() {
         ],
       };
 
-      const convertAPIEndpoint = 'https://v2.convertapi.com/convert/pdf/to/txt?Secret=eR39Od4kzDlMaVXSiFOSY6Tfe42vSk6L';
+      const convertAPIEndpoint = 'https://v2.convertapi.com/convert/pdf/to/txt?Secret=GuB4EualMRQhlrfGZ0LvmO9o7gaCq87z';
       const uploadResponse = await axios.post(convertAPIEndpoint, requestBody, {
         headers: {
           'Content-Type': 'application/json',
